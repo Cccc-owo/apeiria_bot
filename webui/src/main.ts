5cd08e5e2fab7154d24bc8f43385313220bbe20f
Cccc_ <me@iscccc.eu.org>
2026-08-14 14:19:02 +0800

    feat: use system font stack instead of webfonts

--- webui/src/main.ts
+++ webui/src/main.ts
@@ -2,14 +2,6 @@ import { createApp } from "vue";
 
 import "vue-sonner/style.css";
 
-import "@fontsource/inter/400.css";
-import "@fontsource/inter/500.css";
-import "@fontsource/inter/600.css";
-import "@fontsource/inter/700.css";
-import "@fontsource/noto-sans-sc/400.css";
-import "@fontsource/noto-sans-sc/500.css";
-import "@fontsource/noto-sans-sc/700.css";
-
 import { createPinia } from "pinia";
 import piniaPersist from "pinia-plugin-persistedstate";
 
